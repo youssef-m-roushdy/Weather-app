356168b9c621f449bf1e68889082a1cc66b2b391
Youssef Mahmoud Roushdy <114890645+youssef-m-roushdy@users.noreply.github.com>
2024-03-13 17:52:14 +0200

    Weather Application

---
+++ main.js
@@ -0,0 +1,43 @@
+const apiKey = "4378d7d93daf0cf4b30ec23222d4cd35"
+const apiUrl = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`
+
+const searchBox = document.querySelector(".search input")
+const searchbtn = document.querySelector(".search button")
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city){
+    const response = await fetch(apiUrl+ city +`&appid=${apiKey}`)
+    var data = await response.json()
+    console.log(data)
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°c";
+    document.querySelector(".humidity").innerHTML = data.main.humidity +"%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    if (data.weather[0].main == "Clouds") {
+        weatherIcon.src = "./images/clouds.png"
+    }
+    else if(data.weather[0].main == "Clear") {
+        weatherIcon.src = "./images/clear.png"
+    }
+    else if(data.weather[0].main == "Rain") {
+        weatherIcon.src = "./images/rain.png"
+    }
+    else if(data.weather[0].main == "Drizzle") {
+        weatherIcon.src = "./images/drizzle.png"
+    }
+    else if(data.weather[0].main == "Mist") {
+        weatherIcon.src = "./images/mist.png"
+    }
+    else if(data.weather[0].main == "Snow") {
+        weatherIcon.src = "./images/snow.png"
+    }
+
+}
+
+
+searchbtn.addEventListener('click', () => {
+    checkWeather(searchBox.value)
+})
+
